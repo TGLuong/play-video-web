716bca6d0e2619a1f16dd579b28e4ccfd1064c8d
gialuong <gialuong9820@gmail.com>
2024-04-08 14:45:48 +0700

    first commit

---
+++ function.js
@@ -0,0 +1,33 @@
+function uploadFile(file_index) {
+    console.log("upload ", file_index)
+    let fileInput = document.getElementById('uploadinput');
+    let file = fileInput.files[0]
+
+    if (!file) {
+      alert("Please select a file.");
+      return;
+    }
+
+    
+    console.log(file)
+    let formData = new FormData();
+    formData.append('folder_path', file_index)
+    formData.append('file', file);
+
+    fetch('http://pi.local:5000/api/file', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to upload file.');
+        }
+        return response.json();
+    })
+    .then(data => {
+        alert('File uploaded successfully');
+    })
+    .catch(error => {
+        alert('Error uploading file');
+    });
+}
